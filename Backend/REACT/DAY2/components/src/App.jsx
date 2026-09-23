97075bb3e4e59742fc7a8bb8e461c5d073007e9c
Lalli <lallisree5@gmail.com>
2026-09-23 15:53:39 +0530

    day1

---
+++ Backend/REACT/DAY2/components/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import About from './components/About'
+
+const App = () => {
+  return (<>
+    <div>
+      
+      <Navbar/>
+      <Banner/>
+      <About/>
+
+    </div>
+  </>)
+}
+
+export default App
