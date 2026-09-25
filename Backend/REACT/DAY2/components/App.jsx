92849494f056ebd4622e162ddb415453029b4d5c
lallisree <lallisree5@gmail.com>
2026-09-25 09:58:14 +0530

    Add files via upload

---
+++ Backend/REACT/DAY2/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (<>
+    <div>
+      
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Footer/>
+
+
+    </div>
+  </>)
+}
+
+export default App
